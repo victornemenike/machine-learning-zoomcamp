c6a9d622cbf064fca4ffcb6726bff43951361865
victornemenike <11035953+victornemenike@users.noreply.github.com>
2024-10-29 23:23:17 +0000

    complete questions 5 & 6 on Docker

---
+++ 05-deployment/predict_docker.py
@@ -0,0 +1,36 @@
+import pickle
+
+from flask import Flask, request, jsonify
+
+def predict_single(customer, dv, model):
+    X = dv.transform([customer])
+    y_pred = model.predict_proba(X)[0, 1]
+    return y_pred
+
+model_file = 'model2.bin'
+dv_file = 'dv.bin'
+
+with open(model_file, 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open(dv_file, 'rb') as f_in:
+    dv = pickle.load(f_in)
+
+app = Flask('churn')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    prediction = predict_single(customer, dv, model)
+    churn = prediction >= 0.5
+
+    result = {
+        'churn_probability': float(prediction),
+        'churn': bool(churn)
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug = True, host = '0.0.0.0', port = 9696)
